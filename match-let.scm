5da758e4303915fd03dfc082c8cf77cde0de854f
JaneEyre446 <ramseyer@mit.edu>
2015-05-02 16:56:59 -0400

    match let code as it

--- match-let.scm
+++ match-let.scm
@@ -1,40 +1,77 @@
 ;;The code for let.  
-#|(define-syntax m-let
-  (syntax-rules ()
-    ((_ (vars) body ...)
-     (let ((letvars (map car vars))
-	   (letexprs (map cadr vars)))
-       
-|#
-
-;;dummy test
-'((x 1) (y 2))
-
-(let ((x 1) (y 2))
-  (pp x)
-  (pp y))
-
-(define testlist ((x 1) (y 2)))
-
-#|
-(let (((caar testlist) (cadar testlist))
-      ((caadr testlist) (cdadr testlist)))
-  (pp "here"))
-|#
 
 (define-syntax dict-let
   (sc-macro-transformer
    (lambda (exp env)
-     (let* ((dict (cdr exp)))
-       (pp
-	`(begin
-	   ,@(let (
-		   ,@((map (lambda(entry)
-			     (let ((val 
-			      (make-syntactic-closure env '() (cadr entry))))
-			       `(,(car entry) ,val))))
-		  dict)))))))))
-
+     (let ((dict (cadr exp))
+	    (body (caddr exp)))
+      ;(pp dict)
+     ; (pp
+	`(let (
+	       ,@(map (lambda(entry)
+;			     (let ((val 
+;			      (make-syntactic-closure env '() (cadr entry))))
+			       `(,(car entry) ,(cadr entry)))
+		  dict))
+	   ,body)))));)
 
 (define d 4)
-(dict-let (a d) (b cos) (c 3))
+(dict-let ((a d) (b cos) (c 3)) (+ c a))
+;7 
+;Success!!
+
+#|
+;For when we get a generic version working
+(define testAList '((a d) (b cos) (c 3)))
+(dict-let testAList (+ c a))
+|#
+(let ((((? x) (? y)) (1 2))
+      (c 4))
+  stuff)
+
+(define-syntax match-let
+  (sc-macro-transformer
+   (lambda (exp env)
+     (let* ((match (cadr exp))
+	    (vals (caddr exp))
+	    (body (cadddr exp))
+	    (dict ((match:->combinators match)
+				       vals 
+				       '() 
+				       (lambda (d n) d))))
+       (if dict
+	   `(let (
+		  ,@(map (lambda(entry)
+			;   (let ((val 
+			;	  (make-syntactic-closure env '() (cadr entry))))
+			     `(,(car entry) ,(cadr entry)))
+			 dict))
+	   ,body)
+	   (pp 'failed-match))))))
+
+(match-let ((? y) (? x)) ((1 2)) (+ x y))
+;3 
+;Success!
+(match-let ((? y) (? x)) ((1)) (+ x y))
+;failed-match 
+;Success!
+
+(define-syntax dict-let*
+  (sc-macro-transformer
+   (lambda (exp env)
+     (let ((dict (cadr exp))
+	    (body (caddr exp)))
+      ;(pp
+	`(let* (
+	       ,@(map (lambda(entry)
+			    ; (let ((val 
+			    ;  (make-syntactic-closure env '() (cadr entry))))
+			       `(,(car entry) ,(cadr entry)));)
+		  dict))
+	   ,body)))));)
+
+(dict-let* ((a 4) (b cos) (c a)) (+ c a))
+;8 
+;Success!
+
+;No match-let* because currently it will be useless
