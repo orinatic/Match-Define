02f7e08038374ea1b0839fe90446ff5aa990b76e
Eli Davis <ebdavis@mit.edu>
2015-05-02 18:09:09 -0400

    match-let and match-let*

--- match-let.scm
+++ match-let.scm
@@ -29,32 +29,71 @@
       (c 4))
   stuff)
 
+ 
+(match-let ((((? y) (? x)) ((1 2)))
+	     (d 5))
+	   (+ d x y))
+
+(match-let ((((? y)) ((1 2)))
+	     );(d 5))
+	   (+ x y))
+
+(match-let ((((? y)) ((4 2)))
+	     (d 5))
+	   (pp d)
+	   (pp x)
+	   (pp y)
+	   d)
+
+(define (*run-match* vars pattern)
+  ((match:->combinators vars) pattern '() (lambda (d n) d)))
+
 (define-syntax match-let
   (sc-macro-transformer
    (lambda (exp env)
-     (let* ((match (cadr exp))
-	    (vals (caddr exp))
-	    (body (cadddr exp))
-	    (dict ((match:->combinators match)
-				       vals 
-				       '() 
-				       (lambda (d n) d))))
-       (if dict
-	   `(let (
-		  ,@(map (lambda(entry)
-			;   (let ((val 
-			;	  (make-syntactic-closure env '() (cadr entry))))
-			     `(,(car entry) ,(cadr entry)))
-			 dict))
-	   ,body)
-	   (pp 'failed-match))))))
+     (let* ((assigns (cadr exp))
+	    (body (cddr exp))
+	    (dict 
+	     (filter
+	      pair?
+	      (let assign-iter ((todo assigns) (done '()))
+		(if (null? todo)
+		    done
+		    (let* ((assign (car todo))
+			   (vars (car assign))
+			   (vals (cadr assign)))
+		      (if (identifier? vars)
+			  (assign-iter 
+			   (cdr todo) 
+			   (append done (list assign)))
+			  (assign-iter
+			   (cdr todo)
+			   (append done (or (*run-match* vars vals)
+					    (begin 
+					      (warn 'match-failed-in-let)
+					      '())))))))))))
+       `(let ( ,@(map (lambda(entry)
+			`(,(car entry) ,(cadr entry)))
+		      dict))
+	   (begin ,@(map (lambda (statement)
+		    statement)
+		  body))) 
+	   ))))
 
-(match-let ((? y) (? x)) ((1 2)) (+ x y))
-;3 
-;Success!
-(match-let ((? y) (? x)) ((1)) (+ x y))
-;failed-match 
-;Success!
+
+ 
+(match-let ((((? y) (? x)) ((1 2)))
+	     (d 5))
+	   (+ d x y)) ;-> 8
+
+(match-let ((((? y) (? x)) ((1 2))))
+	   (+ x y)) ;-> 3
+
+(match-let ((((? r)) ((4 2)))
+	     (d 5))
+	   d)
+;-> Warning: match-failed-in-let
+;5
 
 (define-syntax dict-let*
   (sc-macro-transformer
@@ -75,3 +114,53 @@
 ;Success!
 
 ;No match-let* because currently it will be useless
+
+
+(define-syntax match-let*
+  (sc-macro-transformer
+   (lambda (exp env)
+     (let* ((assigns (cadr exp))
+	    (body (cddr exp))
+	    (dict 
+	     (filter
+	      pair?
+	      (let assign-iter ((todo assigns) (done '()))
+		(if (null? todo)
+		    done
+		    (let* ((assign (car todo))
+			   (vars (car assign))
+			   (vals (cadr assign)))
+		      (if (identifier? vars)
+			  (assign-iter 
+			   (cdr todo) 
+			   (append done (list assign)))
+			  (assign-iter
+			   (cdr todo)
+			   (append done (or (*run-match* vars vals)
+					    (begin 
+					      (warn 'match-failed-in-let)
+					      '())))))))))))
+       (pp dict)
+       `(let* ( ,@(map (lambda(entry)
+			`(,(car entry) ,(cadr entry)))
+		      dict))
+	   (begin ,@(map (lambda (statement)
+		    statement)
+		  body))) 
+	   ))))
+
+ 
+(match-let* ((((? y) (? x)) ((1 2)))
+	     (d (+ y x)))
+	    (+ d x y)) ;-> 6
+
+(match-let* ((((? y) (? x)) ((1 2)))
+	    (((? c) (? d)) (((+ 3 x) y))))
+	   (+ c d x y)) ;-> 9
+
+(match-let ((? y) (? x)) ((1 2)) (+ x y))
+;3 
+;Success!
+(match-let ((? y) (? x)) ((1)) (+ x y))
+;failed-match 
+;Success!
