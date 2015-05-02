faa6919911c852534ec23743e435b4d4be3f5015
JaneEyre446 <ramseyer@mit.edu>
2015-05-02 14:52:23 -0400

    match let

--- match-let.scm
+++ match-let.scm
@@ -14,9 +14,27 @@
   (pp x)
   (pp y))
 
-(define testlist '((x 1) (y 2)))
+(define testlist ((x 1) (y 2)))
 
-
+#|
 (let (((caar testlist) (cadar testlist))
       ((caadr testlist) (cdadr testlist)))
   (pp "here"))
+|#
+
+(define-syntax m-let
+  (sc-macro-transformer
+   (lambda (exp env)
+     (let* ((values (cdr exp))
+	    (vars (map car values))
+	    (exprs (map cadr values)))
+      (pp vars)
+      (pp exprs)
+       (pp
+       `(begin
+	   ,@(map (lambda(v e)
+		    (let ((w (make-syntactic-closure env '() v)))
+		      `(define ,v ,e)))
+		 vars exprs)))))))
+
+(m-let '((a 1) (b 1) (c 1)))
