4b339c8dfe48f05b9e746776cf6a3ea50432e5ec
JaneEyre446 <ramseyer@mit.edu>
2015-05-05 21:01:30 -0400

    more tests for match-let-final

--- match-let-final.scm
+++ match-let-final.scm
@@ -45,3 +45,101 @@
 		    body)) 
 	   ,@(map (lambda (var) 
 		    `(match:lookup *d* ',var)) vars))))))))
+;Match-let testing
+
+(match-let '(5 6) '((? a) (? d))
+	      (+ a d))
+;11
+
+(match-let '(1 2 3 4) '((? a) (? b) (? c) 4)
+	      (match-let '(5 6) '((? a) (? d))
+			       (+ a b c d)))
+;16
+
+(let ((token '(bin-arith + 2 4)))
+  (match-let token `(bin-arith (? op) (? a1 ,number?) (? a2 ,number?))
+	          (pp (list op a1 a2))))
+
+;(+ 2 4) ;Unspecified return value
+
+(match-let '(1 2) '((? a) (? b)) (+ a b)) 
+;3
+
+(let ((vals '(seq 1 2 3 4 5)))
+ (match-let vals '(seq (? x) (?? xs)) (cons x xs)))
+;(1 2 3 4 5)
+
+(let ((vals '(seq 1 2 3 4 5)))
+ (match-let vals '(seq (? x) (?? xs)) (pp `(,x and ,xs have been set))
+	    (cons x xs)))
+; (1 and (2 3 4 5) have been set)
+;Value 20: (1 2 3 4 5)
+
+
+;;;Testing multi-line body 
+
+(let ((vals '(seq 1 2 3 4 5)))
+ (match-let vals '(seq (? x) (?? xs)) (begin (cons x xs) (pp 'herewego) 
+			     (pp 'lotsoflines)
+			     (pp x) (pp xs))))
+;herewego
+;lotsoflines
+;1
+;(2 3 4 5)
+;;Unspecified return value
+
+
+(let ((vals '(seq 1 2 3 4 5)))
+ (match-let vals '(seq (? x) (?? xs)) (pp (cons x xs)) (pp 'herewego) (pp x) (pp xs)))
+;(1 2 3 4 5)
+;herewego
+;1
+;(2 3 4 5)
+;;Unspecified return value
+;;Yay!   Works without the begin statement.  
+
+;;;Testing predicates
+
+((matcher '(a ((? b) 2 3) (? b) c))
+ '(a (1 2 3) 1 c))
+;((b 1))
+
+((matcher '((? b)))
+ '(4))
+;((b 4))
+
+((matcher `((? b ,number?)))
+ '(4))
+;((b 4))
+
+((matcher `((? x ,number?) (?? xs))) '(1 2 3 4 5))
+;((xs (2 3 4 5)) (x 1))
+
+(let ((vals '(1 2 3 4 5)))
+ (match-let vals `((? x ,number?) (?? xs)) (cons x xs)))
+;(1 2 3 4 5)
+
+((matcher `((? x ,number?) (?? xs ,number?))) '(1 2 3 4 5))
+;((xs (2 3 4 5)) (x 1))
+
+(let ((vals '(1 2 3 4 5)))
+ (match-let vals `((? x ,number?) (?? xs ,number?)) (cons x xs)))
+;(1 2 3 4 5)
+
+;Segment variables don't work with predicates. This is undefined
+;behavior and is weird but okay
+((matcher `((? x ,number?) (?? xs ,number?))) '(1 #f #f #f #f))
+;((xs (#f #f #f #f)) (x 1))
+
+;same as above
+(let ((vals '(1 2 3 4 #t)))
+ (match-let vals `((? x ,number?) (?? xs ,number?)) (cons x xs)))
+;(1 2 3 4 #t)
+
+
+(let ((vals '(1 #t 3 4 5)))
+ (match-let vals `((? x ,boolean?) (?? xs)) (cons x xs)))
+;errors, because match failed.  
+
+((matcher `((? x ,boolean?) (?? xs))) '(1 #t 3 4 5))
+;#f
